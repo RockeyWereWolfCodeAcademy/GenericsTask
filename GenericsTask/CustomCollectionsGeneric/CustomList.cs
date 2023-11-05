2c4e930fecd3247762d4b15ae6216410d726ed2b
RockeyWereWolf <mi71k9d7p@code.edu.az>
2023-11-05 18:11:42 +0400

    uploaded project

---
+++ GenericsTask/CustomCollectionsGeneric/CustomList.cs
@@ -0,0 +1,153 @@
+﻿namespace CustomCollectionsGeneric
+{
+    public class CustomList<T>
+    {
+        T[] _array;
+        int _count;
+        const int DefaultCapacity = 5;
+        public int Count { get => _count; }
+        public int Capacity { get => _array.Length; 
+            set
+            {
+                if (value < _count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if (value > 0)
+                {
+                    _array = new T[value];
+                    T[] tempArray = new T[value];
+                    if (_count > 0)
+                    {
+                        Array.Copy(_array, tempArray, _count);
+                    }
+                    _array = tempArray;
+                }
+                else
+                {
+                    _array = new T[value];
+                }
+                
+            } 
+        }
+
+        public CustomList()
+        {
+            _array = new T[0];
+        }
+
+        public CustomList(int size)
+        {
+            if (size < 0)
+                throw new ArgumentOutOfRangeException("Size must be positive");
+            Capacity = size == 0 ? DefaultCapacity : size;
+            //_array = new T[Capacity];    
+        }
+
+        public CustomList(params T[] array)
+        {
+            int size = array.Length;
+            Capacity = size == 0 ? DefaultCapacity : size;
+            _count = size;
+            //_array = new T[Capacity];
+            Array.Copy(array, _array, size);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0) throw new IndexOutOfRangeException("Index cannot be negative");
+                if (index > Count) throw new IndexOutOfRangeException("Index cannot be bigger than elements count");
+                return _array[index];
+            }
+            set
+            {
+                if (index < 0) throw new IndexOutOfRangeException("Index cannot be  negative");
+                if (index > Count) throw new IndexOutOfRangeException("Index cannot be bigger than elements count");
+                _array[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        { 
+            if (_count == Capacity)
+            {
+                Array.Resize(ref _array, _count + Capacity);
+            }
+            _array[_count++] = item;
+        }
+
+        public void Clear()
+        {
+            _array = new T[DefaultCapacity];
+            //Capacity = 0;
+            _count = 0;
+        }
+        public bool Exist(T item)
+        {
+            foreach (var element in _array)
+            {
+                if (element.Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (_array[i].Equals(item)) // Use custom equality logic if needed
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int LastIndexOf(T item)
+        {
+            for (int i = _array.Length - 1; i >= 0; i--)
+            {
+                if (_array[i].Equals(item)) // Use custom equality logic if needed
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                for (int i = index; i < _array.Length - 1; i++)
+                {
+                    _array[i] = _array[i + 1];
+                }
+                Array.Resize(ref _array, _array.Length - 1);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reverse()
+        {
+            int left = 0;
+            int right = _count - 1;
+            while (left < right)
+            {
+                T temp = _array[left];
+                _array[left] = _array[right];
+                _array[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
---
+++ GenericsTask/GenericsTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using CustomCollectionsGeneric;
+using System.Reflection;
+
+namespace GenericsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> ints = new CustomList<int>(1, 2, 3);
+            Console.WriteLine(ints.Capacity);
+            Console.WriteLine(ints.Count);
+            ints.Add(4);
+            Console.WriteLine(ints.Count);
+            Console.WriteLine(ints.Capacity);
+            ints.Add(4);
+            Console.WriteLine(ints.Count);
+            Console.WriteLine(ints.Capacity);
+            ints.Add(6);
+            Console.WriteLine(ints.Count);
+            Console.WriteLine(ints.Capacity);
+            ints.Add(7);
+            Console.WriteLine(ints.Count);
+            Console.WriteLine(ints.Capacity);//12
+            ints.Clear();
+            Console.WriteLine(ints.Count);
+            Console.WriteLine(ints.Capacity);
+            ints.Add(1);
+            Console.WriteLine(ints.Count);
+            Console.WriteLine(ints.Capacity);
+            ints.Add(1);
+            Console.WriteLine(ints.Count);
+            Console.WriteLine(ints.Capacity);
+            Console.WriteLine(ints.IndexOf(1));
+            ints.Add(2);
+            Console.WriteLine(ints.Count);
+            Console.WriteLine(ints.Capacity);
+            ints.Reverse();
+            Console.WriteLine(ints[0]);//2
+        }
+    }
+}
